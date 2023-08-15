6109535a2aba38a38ec911bf3da8a325b556cc27
Roshan Kern <roshan.kern@ucdenver.edu>
2023-08-15 08:09:09 -0700

    add onboarding screen

---
+++ lib/widgets/settings/onboarding_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  @override
+  _OnboardingScreenState createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('hello'),
+      ),
+    );
+  }
+}
