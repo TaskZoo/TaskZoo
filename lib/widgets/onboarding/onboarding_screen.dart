f6c6240093f09e31b9d013ef43f725e5caa63115
Roshan Kern <roshan.kern@ucdenver.edu>
2023-08-15 19:26:22 -0700

    updated onboard page content

--- lib/widgets/onboarding/onboarding_screen.dart
+++ lib/widgets/onboarding/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dimensions_theme/dimensions_theme.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:taskzoo/widgets/onboarding/custom_nav_bar.dart';
 import 'package:taskzoo/widgets/onboarding/onboarding_page.dart';
@@ -23,6 +24,160 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Widget onboarding_page_1 = OnboardingPage(
+        title: "Create Tasks",
+        topBackgroundColor: "#FFA06B",
+        animalSvgPath: "assets/onboarding/llama.svg",
+        content: [
+          const Text(
+            "Create tasks for goals that you want to achieve and filter by tags to keep everything organized.",
+            style: TextStyle(fontSize: 20),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Icon(Icons.keyboard_control),
+            Container(
+              width: 1,
+              height: 40,
+              color: Theme.of(context).indicatorColor,
+              margin: EdgeInsets.symmetric(
+                  horizontal: Dimensions.of(context).insets.smaller),
+            ),
+            const Icon(Icons.add),
+          ]),
+          const Text(
+            "Use daily, weekly, and monthly tasks to encourage productivity over different time periods.",
+            style: TextStyle(fontSize: 20),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Theme.of(context).indicatorColor,
+                  width: Dimensions.of(context).borderWidths.small,
+                ),
+                color: Theme.of(context).cardColor,
+              ),
+              child: Center(
+                child: Text(
+                  "D",
+                  style: TextStyle(
+                    color: Theme.of(context).indicatorColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: Dimensions.of(context).insets.small),
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Theme.of(context).indicatorColor,
+                  width: Dimensions.of(context).borderWidths.small,
+                ),
+                color: Theme.of(context).cardColor,
+              ),
+              child: Center(
+                child: Text(
+                  "W",
+                  style: TextStyle(
+                    color: Theme.of(context).indicatorColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: Dimensions.of(context).insets.small),
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Theme.of(context).indicatorColor,
+                  width: Dimensions.of(context).borderWidths.small,
+                ),
+                color: Theme.of(context).cardColor,
+              ),
+              child: Center(
+                child: Text(
+                  "M",
+                  style: TextStyle(
+                    color: Theme.of(context).indicatorColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ]),
+        ]);
+
+    Widget onboarding_page_2 = OnboardingPage(
+        title: "Use Tasks",
+        topBackgroundColor: "#45DAFF",
+        animalSvgPath: "assets/onboarding/flamingo.svg",
+        content: [
+          const Text(
+            "Hold down a task card to mark it as complete, or tap the task card to modify/delete the task.",
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            color: Theme.of(context).scaffoldBackgroundColor,
+            padding: EdgeInsets.all(Dimensions.of(context).insets.medium),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [Text("Card front"), Text("Card back")],
+            ),
+          )
+        ]);
+
+    Widget onboarding_page_3 = OnboardingPage(
+        title: "Collect Pieces",
+        topBackgroundColor: "#85FF91",
+        animalSvgPath: "assets/onboarding/parrot.svg",
+        content: [
+          const Text(
+            "Get rewarded with pieces every time you complete a task!",
+            style: TextStyle(fontSize: 20),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(FontAwesomeIcons.puzzlePiece),
+              SizedBox(width: Dimensions.of(context).insets.large),
+              Text("Number increase animation")
+            ],
+          ),
+          const Text(
+            "The amount of pieces you receive depends on how big a task you complete.",
+            style: TextStyle(fontSize: 20),
+          ),
+        ]);
+
+    Widget onboarding_page_4 = OnboardingPage(
+        title: "Build Animals",
+        topBackgroundColor: "#FFEDA8",
+        animalSvgPath: "assets/onboarding/lion.svg",
+        content: [
+          const Text(
+            "Tapping an animal that is incomplete will use pieces to build that animal.",
+            style: TextStyle(fontSize: 20),
+          ),
+          const Text(
+            "Once an animal is complete, it becomes part of your zoo!",
+            style: TextStyle(fontSize: 20),
+          ),
+        ]);
+
     return Scaffold(
       body: PageView(
         controller: _pageController,
@@ -32,131 +187,10 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           });
         },
         children: [
-          OnboardingPage(
-              title: "Create Tasks",
-              topBackgroundColor: "#FFA06B",
-              animalSvgPath: "assets/onboarding/llama.svg",
-              content: [
-                const Text(
-                  "Create tasks for goals that you want to achieve and filter by tags to keep everything organized.",
-                  style: TextStyle(fontSize: 20),
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  const Icon(Icons.keyboard_control),
-                  Container(
-                    width: 1,
-                    height: 40,
-                    color: Theme.of(context).indicatorColor,
-                    margin: EdgeInsets.symmetric(
-                        horizontal: Dimensions.of(context).insets.smaller),
-                  ),
-                  const Icon(Icons.add),
-                ]),
-                const Text(
-                  "Use daily, weekly, and monthly tasks to encourage productivity over different time periods.",
-                  style: TextStyle(fontSize: 20),
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Theme.of(context).indicatorColor,
-                        width: Dimensions.of(context).borderWidths.small,
-                      ),
-                      color: Theme.of(context).cardColor,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "D",
-                        style: TextStyle(
-                          color: Theme.of(context).indicatorColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: Dimensions.of(context).insets.small),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Theme.of(context).indicatorColor,
-                        width: Dimensions.of(context).borderWidths.small,
-                      ),
-                      color: Theme.of(context).cardColor,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "W",
-                        style: TextStyle(
-                          color: Theme.of(context).indicatorColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: Dimensions.of(context).insets.small),
-                  Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Theme.of(context).indicatorColor,
-                        width: Dimensions.of(context).borderWidths.small,
-                      ),
-                      color: Theme.of(context).cardColor,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "M",
-                        style: TextStyle(
-                          color: Theme.of(context).indicatorColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ]),
-              ]),
-          OnboardingPage(
-              title: "Use Tasks",
-              topBackgroundColor: "#45DAFF",
-              animalSvgPath: "assets/onboarding/flamingo.svg",
-              content: [
-                const Text(
-                  "Hold down a task card to mark it as complete, or tap the task card to modify/delete the task.",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ]),
-          OnboardingPage(
-              title: "Collect Pieces",
-              topBackgroundColor: "#85FF91",
-              animalSvgPath: "assets/onboarding/parrot.svg",
-              content: [
-                const Text(
-                  "Get rewarded with pieces every time you complete a task!",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ]),
-          OnboardingPage(
-              title: "Build Animals",
-              topBackgroundColor: "#FFEDA8",
-              animalSvgPath: "assets/onboarding/lion.svg",
-              content: [
-                const Text(
-                  "Tapping an animal that is incomplete will use pieces to build that animal.",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ]),
+          onboarding_page_1,
+          onboarding_page_2,
+          onboarding_page_3,
+          onboarding_page_4
         ],
       ),
       bottomNavigationBar: CustomNavBar(
